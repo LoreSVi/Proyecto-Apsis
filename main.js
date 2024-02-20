8bc5c423f64f5ec23658fcafa74d454c08d20bb6
LoreSVi <loresuvivas@gmail.com>
2024-02-20 06:52:59 -0300

    estilo y javaScript

---
+++ main.js
@@ -0,0 +1,14 @@
+/*
+let dolar =document.getElementById("dolar")
+let euros = document.getElementById("euros")
+let libras= document.getElementById("libras")
+
+dolar.onclick = () => {
+    let precioDolar = miDinero*0.0012
+
+let print = document.createElement("h3")
+print.innerHTML =  `${precioDolar} USD `
+document.contenedor.appendChild(print)
+}
+
+*/
