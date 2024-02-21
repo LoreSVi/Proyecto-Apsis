df83a415cd1230ba365baf60424b2fcc62009643
LoreSVi <loresuvivas@gmail.com>
2024-02-20 23:20:08 -0300

    calculador de moneda

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-/*
-let dolar =document.getElementById("dolar")
-let euros = document.getElementById("euros")
-let libras= document.getElementById("libras")
 
-dolar.onclick = () => {
-    let precioDolar = miDinero*0.0012
+function calcularPrecio() {
+    // Obtener el importe y la moneda seleccionada
+    let cantidad = parseFloat(document.getElementById('cantidad').value);
+    let moneda = document.getElementById('moneda').value;
 
-let print = document.createElement("h3")
-print.innerHTML =  `${precioDolar} USD `
-document.contenedor.appendChild(print)
+    //  conversión moneda
+    let conversionRates = {
+        usd: 0.0012, 
+        eur: 0.0011, 
+        lib:  0.00095  
+    };
+
+    // Calcular el precio en la moneda seleccionada
+    let convertedPrice = cantidad * conversionRates[moneda];
+
+    // Mostrar el resultado
+    document.getElementById('result').innerHTML = "Precio en " + moneda.toUpperCase() + ": " + convertedPrice.toFixed(2);
 }
-
-*/
